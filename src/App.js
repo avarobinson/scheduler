cf4bd445f697fa27810b8b4f294fbd1f0630ac49
Ava Robinson <avamrobinson@gmail.com>
2020-04-11 22:42:41 -0700

    the real Slice 5 (accidentally named the last one Slice 5, but that was Slice 4

--- src/App.js
+++ src/App.js
@@ -8,10 +8,42 @@ const Banner = ({ title }) => (
   <Title>{ title || '[loading...]' }</Title>
 );
 
-const CourseList = ({ courses }) => (
-  <Button.Group>
-    { courses.map(course => <Course key={course.id} course={ course } />) }
+const CourseList = ({ courses }) => {
+  const [term, setTerm] = useState('Fall');
+  const [selected, toggle] = useSelection();
+  const termCourses = courses.filter(course => term === getCourseTerm(course));
+
+  return (
+    <React.Fragment>
+      {/* /* react fragment groups several components into one without a div because JSX only allows one component to be returned */}
+      {/* passing the state. outer brackets = tell JSX to insert value, innerbracket: make the state an object */}
+      <TermSelector state={ { term, setTerm} } />
+      <Button.Group>
+        { termCourses.map(course => 
+            <Course key={course.id} course={ course } 
+              state={ { selected, toggle } } />) }
+      </Button.Group>
+    </React.Fragment>
+  );
+};
+
+const TermSelector = ({ state }) => (
+  // hasAddons prop just makes buttons connected 
+  <Button.Group hasAddons>
+    {
+      Object.values(terms)
+        .map(value => <Button key={value}
+                              color={ buttonColor(value === state.term)}
+                              onClick={ () => state.setTerm(value) }
+                              >
+                              { value }
+                      </Button>)
+    }
   </Button.Group>
+);
+
+const buttonColor = selected => (
+  selected ? 'success' : null
 );
 
 const getCourseTerm = course => (
@@ -22,11 +54,77 @@ const getCourseNumber = course => (
   course.id.slice(1, 4)
 )
   
-const Course = ({ course }) => (
-  <Button>
+const Course = ({ course, state }) => (
+  <Button color={ buttonColor(state.selected.includes(course))}
+    onClick={ () => state.toggle(course) }
+    disabled={ hasConflict(course, state.selected) }
+  >
     { getCourseTerm(course) } CS { getCourseNumber(course) }: { course.title }
   </Button>
 );
+
+// course conflict code 
+const hasConflict = (course, selected) => (
+  selected.some(selection => courseConflict(course, selection))
+)
+
+const daysOverlap = (days1, days2) => (
+  days.some(day => days1.includes(day) && days2.includes(day))
+);
+
+const hoursOverlap = (hours1, hours2) => (
+  Math.max(hours1.start, hours2.start) < Math.min(hours1.end, hours2.end)
+);
+
+const timeConflict = (course1, course2) => (
+  daysOverlap(course1.days, course2.days) 
+  && hoursOverlap(course1.hours, course2.hours)
+);
+
+// returns true if courses are different but in same term and overlap on some day and time 
+const courseConflict = (course1, course2) => (
+  course1 !== course2
+  && getCourseTerm(course1) === getCourseTerm(course2)
+  && timeConflict(course1, course2)
+);
+
+const days = ['M', 'Tu', 'W', 'Th', 'F'];
+
+// parse out the meeting strings into easier to use data structures 
+const meetsPat = /^ *((?:M|Tu|W|Th|F)+) +(\d\d?):(\d\d) *[ -] *(\d\d?):(\d\d) *$/;
+
+const timeParts = meets => {
+  const [match, days, hh1, mm1, hh2, mm2] = meetsPat.exec(meets) || [];
+  return !match ? {} : {
+    days,
+    hours: {
+      start: hh1 * 60 + mm1 * 1,
+      end: hh2 * 60 + mm2 * 1
+    }
+  };
+};
+
+//  avoid constatly reparsing the meeting strings by adding new fields to each course 
+const addCourseTimes = course => ({
+  ...course,
+  ...timeParts(course.meets)
+});
+
+const addScheduleTimes = schedule => ({
+  title: schedule.title,
+  courses: schedule.courses.map(addCourseTimes)
+});
+
+// behaves like useState - return two values, state value and setting function
+// state setting function (toggle) will set the state to a copied list with a new element or an element removed 
+// custom hook : calls a hook function (must begin with use)
+const useSelection = () => {
+  const [selected, setSelected] = useState([])
+  const toggle = (x) => {
+    setSelected(selected.includes(x) ? selected.filter(y => y !== x) : [x].concat(selected))
+  };
+  return [ selected, toggle ];
+};
 
 const App = () =>  {
   const [schedule, setSchedule] = useState({ title: '', courses: [] });
@@ -40,7 +138,7 @@ const App = () =>  {
     const response = await fetch(url);
     if (!response.ok) throw response;
     const json = await response.json();
-    setSchedule(json);
+    setSchedule(addScheduleTimes(json));
   }
   fetchSchedule();
   }, [])
